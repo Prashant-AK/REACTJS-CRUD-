e4e2d9ea24ed4fc2a1a5142ce32543c3f4464143
Prashant Singh <ps.prashantsingh.98@gmail.com>
2020-06-19 18:25:26 +0530

    Updates

---
+++ precache-manifest.95f376c5ca83372371d0b4900503e7a0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52cc8a6bd46b0a1a88bf697a2371d74d",
+    "url": "/REACTJS-CRUD-/index.html"
+  },
+  {
+    "revision": "8b6f20bf268575a59857",
+    "url": "/REACTJS-CRUD-/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "63ad5780ce384781ec66",
+    "url": "/REACTJS-CRUD-/static/css/main.8440b4f4.chunk.css"
+  },
+  {
+    "revision": "8b6f20bf268575a59857",
+    "url": "/REACTJS-CRUD-/static/js/2.7d358a26.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/REACTJS-CRUD-/static/js/2.7d358a26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63ad5780ce384781ec66",
+    "url": "/REACTJS-CRUD-/static/js/main.7d8f1643.chunk.js"
+  },
+  {
+    "revision": "8919d2879bec4a626081",
+    "url": "/REACTJS-CRUD-/static/js/runtime-main.7a26fab6.js"
+  },
+  {
+    "revision": "973092b09f9b545e00f6e3e043c18724",
+    "url": "/REACTJS-CRUD-/static/media/emoji.973092b0.jpg"
+  }
+]);
